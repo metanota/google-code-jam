3f899b62f907297227c0202a76e96924be9ba1fe
metanota <metanota@gmail.com>
2014-05-06 02:18:37 +0400

    Little styling fixes

--- 2014/qualification/d/Main.scala
+++ 2014/qualification/d/Main.scala
@@ -14,30 +14,30 @@ object Main extends App {
     val naomi = i.next().split(' ').map(_.toDouble).toList.sorted
     val ken   = i.next().split(' ').map(_.toDouble)
 
-    val deceitful = playDeceitful(0, naomi, ken.toList.sorted,        blocksNum)
-    val fair      = playFair     (0, naomi, SortedSet(ken: _*), blocksNum)
+    val deceitful = playDeceitful(naomi, ken.toList.sorted,  blocksNum)
+    val fair      = playFair     (naomi, SortedSet(ken: _*), blocksNum)
     writer println s"Case #$singleTry: $deceitful $fair"
   }
 
   writer.close()
 
   @tailrec
-  def playDeceitful(accum: Int, naomi: Seq[Double], ken: Seq[Double], blocksNum: Int): Int = blocksNum match {
+  def playDeceitful(naomi: Seq[Double], ken: Seq[Double], blocksNum: Int, accum: Int = 0): Int = blocksNum match {
     case 0 => accum
     case _ =>
-      if (naomi.head < ken.head) playDeceitful(accum,     naomi.tail, ken.init, blocksNum - 1)
-      else                       playDeceitful(accum + 1, naomi.tail, ken.tail, blocksNum - 1)
+      if (naomi.head < ken.head) playDeceitful(naomi.tail, ken.init, blocksNum - 1, accum)
+      else                       playDeceitful(naomi.tail, ken.tail, blocksNum - 1, accum + 1)
   }
 
   @tailrec
-  def playFair(accum: Int, naomi: Seq[Double], ken: SortedSet[Double], blocksNum: Int): Int = blocksNum match {
+  def playFair(naomi: Seq[Double], ken: SortedSet[Double], blocksNum: Int, accum: Int = 0): Int = blocksNum match {
       case 0 => accum
       case _ =>
         val naomiStrongest = naomi.last
         val kenStrongest = ken.from(naomiStrongest)
         kenStrongest.isEmpty match {
-          case true  => playFair(accum + 1, naomi.init, ken -          ken.firstKey, blocksNum - 1)
-          case false => playFair(accum,     naomi.init, ken - kenStrongest.firstKey, blocksNum - 1)
+          case true  => playFair(naomi.init, ken -          ken.firstKey, blocksNum - 1, accum + 1)
+          case false => playFair(naomi.init, ken - kenStrongest.firstKey, blocksNum - 1, accum)
         }
   }
 }
